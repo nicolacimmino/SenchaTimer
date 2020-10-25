bf037f580b3c67dfad3005d017a9f4184b6d8024
Nicola Cimmino <nicola.cimmino@gmail.com>
2020-10-25 13:58:34 +0100

    Cleanup

--- SenchaTimer.ino
+++ SenchaTimer.ino
@@ -1,17 +1,47 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+//  Sencha Timer.
+//  Copyright (C) 2014 Nicola Cimmino
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 #include <Arduino.h>
 #include <FastLED.h>
 #include <avr/sleep.h>
 
-#define LED_GND 15
-#define LED_D 16
+/////////////////////////////////////////////////////////////////////////////////////////////
+//  Hardware.
+//
+
+#define LED_GND_PIN 15
+#define LED_DATA_PIN 16
 #define LEDS_COUNT 1
-#define SW_PIN 7
-#define MAX_INFUSIONS 5
 #define TIME_LED_IX 0
+#define BUTTON_PIN 7
 
-CRGB led[LEDS_COUNT];
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
 
-uint16_t infusionTimes[] = {
+/////////////////////////////////////////////////////////////////////////////////////////////
+//  Infusion control.
+//
+
+#define MAX_INFUSIONS 5
+
+uint16_t infusionTimes[MAX_INFUSIONS] = {
     90,
     30,
     45,
@@ -19,18 +49,31 @@ uint16_t infusionTimes[] = {
     180,
 };
 
-uint8_t infusionsCount = 0;
-bool running = false;
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+//  Globals.
+//
+
+CRGB led[LEDS_COUNT];       
+uint8_t infusionsCount = 0; 
 unsigned long infusionStartTime = 0;
 unsigned long pressStartTime = 0;
 
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Setup the LED and initialise the input pin.
+
 void setup()
 {
-  pinMode(LED_GND, OUTPUT);
-  digitalWrite(LED_GND, LOW);
+  pinMode(LED_GND_PIN, OUTPUT);
+  digitalWrite(LED_GND_PIN, LOW);
   delay(300);
 
-  FastLED.addLeds<WS2812B, LED_D, GRB>(led, LEDS_COUNT);
+  FastLED.addLeds<WS2812B, LED_DATA_PIN, GRB>(led, LEDS_COUNT);
   FastLED.setBrightness(70);
 
   for (int ix = 0; ix < LEDS_COUNT; ix++)
@@ -40,8 +83,28 @@ void setup()
 
   FastLED.show();
 
-  pinMode(SW_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
+
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Show the current infustion as a sequence of flahses.
+// The LED is kept dimly lit between flashes to aknoweledge the fact the unit is powered up.
+// This is a non-blocking call, the notification runs on a 3000 mS schedule divided into
+// 200 mS slots which are deterined by the current time:
+// 0    First Flash
+// 1    Dim
+// 2    Second Flash | Dim
+// 3    Dim
+// 4    Third Flash | Dim
+// 5    Dim
+// 6    Forth Flash | Dim
+// 7    Dim
+// 8    Fifth Flash | Dim
+// > 9  Dim
+//
 
 void showCurrentInfusion()
 {
@@ -56,6 +119,16 @@ void showCurrentInfusion()
 
   FastLED.show();
 }
+
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Show that the timer is running.
+// Breaths the LED with a color that shifts from yellow to green as the infustion progresses.
+// This is a non-blocking call, the colour and brightness of the LED depend on the current
+// time and infusion time.
+//
 
 void showTimerRunning()
 {
@@ -72,6 +145,14 @@ void showTimerRunning()
   FastLED.show();
 }
 
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Blink the LED.
+// Used to attract attention blinks the LED between two colors the specificed amount of times.
+// This is a blocking call that will return only once the show completes.
+
 void lightShow(CRGB color1, CRGB color2, uint8_t repetitions, uint16_t delaymS)
 {
   for (uint8_t ix = 0; ix < repetitions; ix++)
@@ -82,21 +163,51 @@ void lightShow(CRGB color1, CRGB color2, uint8_t repetitions, uint16_t delaymS)
   }
 }
 
-void showEnd()
-{
-  lightShow(CRGB::Orange, CRGB::Yellow, 10, 200);
-}
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
 
-void showReset()
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Reset the current infusion.
+// This stops the current infusion but doesn't reset the count. The operation is aknoweledged 
+// by a green/yellow flashing pattern.
+
+void resetCurrentInfusion()
 {
+  infusionStartTime = 0;
+
   lightShow(CRGB::Green, CRGB::Yellow, 10, 200);
 }
 
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// End the current infusion.
+// This ends the infusion and increments the current infusion to the next. The operation is 
+// aknoweledged by a orange/yellow flashing pattern.
+
+void endInfusion()
+{
+  infusionStartTime = 0;
+  infusionsCount = (infusionsCount + 1) % MAX_INFUSIONS;
+
+  lightShow(CRGB::Orange, CRGB::Yellow, 10, 200);
+}
+
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Test the button and take action.
+// A short press while the infusion is not running will start it.
+// A long press will reset stop the infustion and maintain the current infusions count.
+
 void checkBrutton()
 {
-  if (digitalRead(SW_PIN) == HIGH)
+  if (digitalRead(BUTTON_PIN) == HIGH)
   {
     pressStartTime = 0;
+
     return;
   }
 
@@ -105,38 +216,48 @@ void checkBrutton()
     pressStartTime = millis();
   }
 
-  if (digitalRead(SW_PIN) == LOW && millis() - pressStartTime > 1000)
+  if (digitalRead(BUTTON_PIN) == LOW && millis() - pressStartTime > 1000)
   {
-    running = false;
-    showReset();
+    resetCurrentInfusion();
 
     return;
   }
 
-  if (!running)
+  if (infusionStartTime == 0)
   {
-    running = true;
     infusionStartTime = millis();
   }
 }
+
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Check if the infusion has come to the end time.
 
 void checkInfusionEnd()
 {
   if (millis() - infusionStartTime > (1000.0 * infusionTimes[infusionsCount]))
   {
-    running = false;
-    showEnd();
-    infusionsCount = (infusionsCount + 1) % MAX_INFUSIONS;
+    endInfusion();
   }
 }
+
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Main loop.
+// Keep checking the input button and run the timer if needed.
 
 void loop()
 {
   checkBrutton();
 
-  if (!running)
+  if (infusionStartTime != 0)
   {
     showCurrentInfusion();
+    
     return;
   }
 
@@ -144,3 +265,6 @@ void loop()
 
   checkInfusionEnd();
 }
+
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
